cf547127d5fcc85cbf7449db8c17c1c05a835d44
ShabinaAslah <shabinashan16@gmail.com>
2026-05-09 20:26:30 +0530

    Update invertedtriangl.sh

--- exp 24/invertedtriangl.sh
+++ exp 24/invertedtriangl.sh
@@ -4,7 +4,7 @@ for ((i=n; i>=1; i--))
 do
 for ((j=1; j<=i; j++))
 do
-echo -n "* " #prints a star with a space on the same line
+echo -n "* "
 done
-echo #prints a newline and moves the cursor to the next row
+echo
 done
